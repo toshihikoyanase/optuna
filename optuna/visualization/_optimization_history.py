9e05c86cd6f9e1f98e2dadf2769781fb11e91dbc
Toshihiko Yanase <toshihiko.yanase@gmail.com>
2021-05-14 09:57:07 +0900

    Add constraints_func to plot_optimization_history.

--- optuna/visualization/_optimization_history.py
+++ optuna/visualization/_optimization_history.py
@@ -1,5 +1,6 @@
 from typing import Callable
 from typing import Optional
+from typing import Sequence
 
 import numpy as np
 
@@ -23,6 +24,7 @@ def plot_optimization_history(
     *,
     target: Optional[Callable[[FrozenTrial], float]] = None,
     target_name: str = "Objective Value",
+    constraints_func: Optional[Callable[[FrozenTrial], Sequence[float]]] = None,
 ) -> "go.Figure":
     """Plot optimization history of all trials in a study.
 
@@ -59,6 +61,13 @@ def plot_optimization_history(
                 Specify this argument if ``study`` is being used for multi-objective optimization.
         target_name:
             Target's name to display on the axis label and the legend.
+        constraints_func:
+            An optional function that computes the objective constraints. It returns a sequence
+            of :obj:`float` values, and a value strictly larger than 0 means that a constraint
+            is violated. The trials violate the constraints, they are excluded from the best
+            trials and they will be plotted as gray points. See also the references of the
+            samplers which support constraints optimization such as :class:`~optuna.samplers.
+            NSGAIISampler` and :class:`~optuna.integration.BoTorchSampler`.
 
     Returns:
         A :class:`plotly.graph_objs.Figure` object.
@@ -71,13 +80,14 @@ def plot_optimization_history(
 
     _imports.check()
     _check_plot_args(study, target, target_name)
-    return _get_optimization_history_plot(study, target, target_name)
+    return _get_optimization_history_plot(study, target, target_name, constraints_func)
 
 
 def _get_optimization_history_plot(
     study: Study,
     target: Optional[Callable[[FrozenTrial], float]],
     target_name: str,
+    constraints_func: Optional[Callable[[FrozenTrial], Sequence[float]]],
 ) -> "go.Figure":
 
     layout = go.Layout(
@@ -92,29 +102,46 @@ def _get_optimization_history_plot(
         _logger.warning("Study instance does not contain trials.")
         return go.Figure(data=[], layout=layout)
 
+    target_func = lambda t: t.value if target is None else target
+
+    traces = []
+    if constraints_func is not None:
+        feasible_trials = []
+        infeasible_trials = []
+        for trial in trials:
+            if all(c <=0 for c in constraints_func(trial)):
+                feasible_trials.append(trial)
+            else:
+                infeasible_trials.append(trial)
+        trials = feasible_trials
+        if len(infeasible_trials) > 0:
+            traces.append(go.Scatter(
+                x=[t.number for t in infeasible_trials],
+                y=[target_func(t) for t in infeasible_trials],
+                mode="markers",
+                name=f"{target_name} (infeasible)",
+                marker_color='LightGray'
+            ))
+
+    if len(trials) > 0:
+        traces.insert(0,
+            go.Scatter(
+                x=[t.number for t in trials],
+                y=[target_func(t) for t in trials],
+                mode="markers",
+                name=target_name,
+            )
+        )
+
     if target is None:
         if study.direction == StudyDirection.MINIMIZE:
             best_values = np.minimum.accumulate([t.value for t in trials])
         else:
             best_values = np.maximum.accumulate([t.value for t in trials])
-        traces = [
-            go.Scatter(
-                x=[t.number for t in trials],
-                y=[t.value for t in trials],
-                mode="markers",
-                name=target_name,
-            ),
-            go.Scatter(x=[t.number for t in trials], y=best_values, name="Best Value"),
-        ]
-    else:
-        traces = [
-            go.Scatter(
-                x=[t.number for t in trials],
-                y=[target(t) for t in trials],
-                mode="markers",
-                name=target_name,
-            ),
-        ]
+        if len(best_values) > 0:
+            traces.insert(1,
+                go.Scatter(x=[t.number for t in trials], y=best_values, name="Best Value"),              
+            )
 
     figure = go.Figure(data=traces, layout=layout)
 
